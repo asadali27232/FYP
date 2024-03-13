3847b8787fe44a90564761d34add6a687b9b7329
HaroonMalik771 <itsharoonmalik143@gmail.com>
2024-03-13 11:43:19 +0500

    updated

--- streamlit_file.py
+++ streamlit_file.py
@@ -7,17 +7,7 @@ import streamlit as st
 # Define the path to the background image
 background_image_path = 'ecg_bg_12.png'
 
-def plot_ecg_from_file():
-    # Define the directory where the files are located
-    data_dir = 'D:\\Semester 7\\FYP\data\\records500\\00000'
-
-    # File names without extensions
-    record_name = '00001_hr'
-
-    # Complete file paths
-    hea_file = os.path.join(data_dir, record_name)
-    dat_file = os.path.join(data_dir, record_name + '.dat')
-
+def plot_ecg_from_file(hea_file, dat_file):
     # Read ECG data using wfdb
     signals, fields = wfdb.rdsamp(hea_file)
 
@@ -32,7 +22,7 @@ def plot_ecg_from_file():
     num_rows = 12
     num_cols = 1
 
-    fig, axs = plt.subplots(num_rows, num_cols, figsize=(15, 5))
+    fig, axs = plt.subplots(num_rows, num_cols, figsize=(20, 20))
 
     for i, ax in enumerate(axs.flat):
         if i < num_leads:  # Ensure we don't exceed the number of leads
@@ -44,41 +34,43 @@ def plot_ecg_from_file():
             ax.set_xticks([])
             ax.set_yticks([])
 
-            # Remove box around subplot
-            ax.spines['top'].set_visible(False)
-            ax.spines['right'].set_visible(False)
-            ax.spines['bottom'].set_visible(False)
-            ax.spines['left'].set_visible(False)
+            # # Remove box around subplot
+            # ax.spines['top'].set_visible(False)
+            # ax.spines['right'].set_visible(False)
+            # ax.spines['bottom'].set_visible(False)
+            # ax.spines['left'].set_visible(False)
 
     # Load and display the background image
-    img = plt.imread(background_image_path)
-    fig.figimage(img, resize='True', alpha=0.7, zorder=-1)
+    # img = plt.imread(background_image_path)
+    # fig.figimage(img, resize='True', alpha=0.7, zorder=-1)
 
     plt.tight_layout()  # Adjust layout
     return fig
 
+
 def main():
     st.title('ECG Viewer')
 
-    # uploaded_file = st.file_uploader("Upload a .hea file", type="hea")
+    uploaded_hea_file = st.file_uploader("Upload a .hea file", type="hea")
 
-    # if uploaded_file is not None:
-    #     # Save uploaded file
-    #     with open('temp.hea', 'wb') as f:
-    #         f.write(uploaded_file.getvalue())
+    if uploaded_hea_file is not None:
+        # Get the current working directory
+        current_dir = os.getcwd()
 
-        # # Define the directory where the files are located
-        # data_dir = os.path.dirname('temp.hea')
+        # Define the data directory relative to the current working directory
+        data_dir = os.path.join(current_dir, "data", "records", "00000")
 
-        # # Read the uploaded .hea file
-        # record_name = os.path.basename('temp.hea').split('.')[0]
+        # Read the uploaded .hea file
+        record_name = os.path.basename(uploaded_hea_file.name).split('.')[0]
 
-        # # Complete file paths
-        # hea_file = os.path.join(data_dir, record_name)
+        # Construct the file paths
+        hea_file = os.path.join(data_dir, record_name)
+        dat_file = os.path.join(data_dir, record_name + '.dat')
 
-    fig = plot_ecg_from_file()
-
-    st.pyplot(fig)
+        fig = plot_ecg_from_file(hea_file, dat_file)
+        st.pyplot(fig)
 
 if __name__ == "__main__":
-    main()
+    main() 
+
+
