892ce480b2f6e57c9740e962c6d44e2cc26d6489
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2024-10-20 10:33:33 +0500

    Updated

--- web/frontend/cardiographpro/src/app/services/page.js
+++ web/frontend/cardiographpro/src/app/services/page.js
@@ -1,11 +1,15 @@
 // src/app/about/page.js
-import styles from './about.module.css'; // Import the CSS module
+import styles from './services.module.css'; // Import the CSS module
 import Banner from '../../components/Banner'; // Import the Banner component
+import ServicesSection from '@/components/ServicesSection';
+import ProcessSection from '@/components/ProcessSection';
 
-export default function AboutPage() {
+export default function ServicesPage() {
     return (
         <section>
             <Banner heading="Our Services" breadcrumb="Services" />
+            <ServicesSection />
+            <ProcessSection />
         </section>
     );
 }
