9047d72d062d50c590e8e547cf58b3f248ad57fe
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2024-10-21 09:21:54 +0500

    Updated

--- web/frontend/cardiographpro/src/components/UploadSignals.js
+++ web/frontend/cardiographpro/src/components/UploadSignals.js
@@ -2,7 +2,9 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import styles from './UploadSignals.module.css'; // Importing the CSS module
-import defaultECG from '../assets/images/shape-18.png'; // Importing the default ECG image
+import defaultECG from '../assets/images/shape-18.png';
+import Button from './Button'; // Make sure to import the Button component
+import check from '../assets/images/check.png';
 
 const UploadSignals = () => {
     const [heaFile, setHeaFile] = useState(null);
@@ -11,6 +13,23 @@ const UploadSignals = () => {
     const [gender, setGender] = useState('0');
     const [result, setResult] = useState(null);
     const [loading, setLoading] = useState(false);
+
+    // Function to calculate age from DOB
+    const dobToAge = (dob) => {
+        const dobDate = new Date(dob);
+        const nowDate = new Date();
+        let calculatedAge = nowDate.getFullYear() - dobDate.getFullYear();
+        const monthDifference = nowDate.getMonth() - dobDate.getMonth();
+
+        // Adjust age if the birthday hasn't occurred yet this year
+        if (
+            monthDifference < 0 ||
+            (monthDifference === 0 && nowDate.getDate() < dobDate.getDate())
+        ) {
+            calculatedAge--;
+        }
+        setAge(calculatedAge);
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -47,7 +66,7 @@ const UploadSignals = () => {
             <h1 className={styles.mainHeading}>ECG Signal Classification</h1>
             <div className={styles.mainContainer}>
                 <div className={styles.leftContainer}>
-                    <h2>Upload ECG .hea and .dat Files</h2>
+                    <h2>Upload ECG Signals</h2>
                     <form onSubmit={handleSubmit} encType="multipart/form-data">
                         <p>
                             <label htmlFor="id_dat_file">
@@ -73,33 +92,41 @@ const UploadSignals = () => {
                                 onChange={(e) => setHeaFile(e.target.files[0])}
                             />
                         </p>
-                        <p>
-                            <label htmlFor="id_age">Enter Age:</label>
-                            <input
-                                type="number"
-                                name="age"
-                                required
-                                id="id_age"
-                                value={age}
-                                onChange={(e) => setAge(e.target.value)}
-                            />
-                        </p>
-                        <p>
-                            <label htmlFor="id_gender">Select Gender:</label>
-                            <select
-                                name="gender"
-                                id="id_gender"
-                                value={gender}
-                                onChange={(e) => setGender(e.target.value)}>
-                                <option value="0">Male</option>
-                                <option value="1">Female</option>
-                            </select>
-                        </p>
-                        <button type="submit" disabled={loading}>
+                        <div className={styles.ageGender}>
+                            <p>
+                                <label htmlFor="id_age">
+                                    Enter Date of Birth:
+                                </label>
+                                <input
+                                    type="date"
+                                    name="age"
+                                    required
+                                    id="id_age"
+                                    onChange={(e) => dobToAge(e.target.value)}
+                                />
+                            </p>
+                            <p>
+                                <label htmlFor="id_gender">
+                                    Select Gender:
+                                </label>
+                                <select
+                                    name="gender"
+                                    id="id_gender"
+                                    value={gender}
+                                    onChange={(e) => setGender(e.target.value)}>
+                                    <option value="0">Male</option>
+                                    <option value="1">Female</option>
+                                </select>
+                            </p>
+                        </div>
+                        <Button
+                            type="submit"
+                            disabled={loading}
+                            class={styles.button}>
                             Upload
-                        </button>
+                        </Button>
                     </form>
-                    {loading && <p className={styles.loading}>Loading...</p>}
+                    {loading && <p className={styles.loading}>Predicting...</p>}
                 </div>
                 <div className={styles.rightContainer}>
                     {result && (
@@ -109,35 +136,77 @@ const UploadSignals = () => {
                             <ul>
                                 {result.superclass_labels.map(
                                     (label, index) => (
-                                        <li key={index}>{`Label ${
-                                            index + 1
-                                        }: ${label}`}</li>
+                                        <li key={index}>
+                                            <div className={styles.icon}>
+                                                <Image
+                                                    src={check}
+                                                    alt="Icon"
+                                                    width={20}
+                                                    height={20}
+                                                    needed
+                                                    style={{
+                                                        marginRight: '8px',
+                                                        verticalAlign: 'middle',
+                                                    }}
+                                                />
+                                            </div>
+                                            {`Label ${index + 1}: ${label}`}
+                                        </li>
                                     )
                                 )}
                             </ul>
                             <h3>Class Names:</h3>
                             <ul>
                                 {result.class_names.map((name, index) => (
-                                    <li key={index}>{name}</li>
+                                    <li key={index}>
+                                        <div className={styles.icon}>
+                                            <Image
+                                                src={check}
+                                                alt="Icon"
+                                                width={20}
+                                                height={20}
+                                                needed
+                                                style={{
+                                                    marginRight: '8px',
+                                                    verticalAlign: 'middle',
+                                                }}
+                                            />
+                                        </div>
+                                        {name}
+                                    </li>
                                 ))}
                             </ul>
                         </div>
                     )}
                 </div>
             </div>
-            <div className={styles.parentEcgImageContainer}>
-                <div className={styles.ecgImageContainer}>
-                    <h2>ECG Image</h2>
-                    <Image
-                        src={result?.image_url || defaultECG} // Placeholder for the default ECG image
-                        alt="ECG Plot"
-                        layout="responsive"
-                        width={500}
-                        height={500}
-                        className={styles.ecgImage}
-                    />
+            {result?.image_url && ( // Only show when image is available
+                <div className={styles.parentEcgImageContainer}>
+                    <div className={styles.ecgImageContainer}>
+                        <h2>ECG Image</h2>
+                        <Image
+                            src={`${
+                                result.image_url
+                            }?t=${new Date().getTime()}`} // Append a unique timestamp to the image URL
+                            alt="ECG Plot"
+                            layout="responsive"
+                            width={500}
+                            height={500}
+                            className={styles.ecgImage}
+                        />
+                    </div>
+                    <div className={styles.arrowDiv}>
+                        <Image
+                            src={defaultECG}
+                            alt="Default ECG Plot"
+                            layout="responsive"
+                            width={500}
+                            height={500}
+                            className={styles.ecgImage}
+                        />
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 };
