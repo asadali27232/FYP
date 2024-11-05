6d1c096f811a717a147f3a4d745e3ddf2f276003
Asad-Rehman22 <126698178+Asad-Rehman22@users.noreply.github.com>
2024-11-06 01:51:34 +0500

    updated

--- web/backend/ecg_project/ecg_project/settings.py
+++ web/backend/ecg_project/ecg_project/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cardiographpro',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'asad786',
         'HOST': 'localhost',  # or '127.0.0.1'
         'PORT': '5432',       # Default PostgreSQL port
     }
